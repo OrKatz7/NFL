3cb99ed88b38fac10cb0fc87257a3d23a2cc5339
OrKatz7 <Or.Katz1@gmail.com>
2020-12-23 23:22:57 +0200

    Add DatasetRetriever2D

--- cnn2d/datasets.py
+++ cnn2d/datasets.py
@@ -23,7 +23,7 @@ def to_tensor(x, **kwargs):
     """
     return torch.tensor(x.transpose(0, 3, 1, 2).astype('float32'))
 
-class DatasetRetriever(Dataset):
+class DatasetRetriever3D(Dataset):
     """
     3D Dataset Retriever for NFL 2020 
     """
@@ -104,6 +104,87 @@ class DatasetRetriever(Dataset):
                
             img /= 255.0
             
+            if self.transforms:
+                img = self.transforms(image=img)['image']
+            
+            if self.add_channel:
+                return img
+            
+            return img[:,:,:3]
+            
+    def get_path(self, row):
+        return row.image_path
+    
+    
+    
+class DatasetRetriever2D(Dataset):
+    """
+    2D Dataset Retriever for NFL 2020 
+    """
+    def __init__(self, 
+                 df = None,
+                 seq_len = 50,
+                 crop_size = 128,
+                 image_size = 150,
+                 transforms = None,
+                 image_root='train_images',
+                 test=False,
+                 add_channel=True):
+        super().__init__()
+
+        self.df = df
+        self.image_root = image_root
+        self.test = test
+        self.add_channel = add_channel
+        self.image_size = image_size
+        self.crop_size = crop_size
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        row = self.df.iloc[index]
+        img = self.load_image(row.iloc[i])
+        if not self.test:
+            label = torch.tensor(row.impact.to_numpy().reshape(-1,1))
+            return sequence, label
+        return sequence
+        
+    def __len__(self) -> int:
+        return self.df
+
+    def load_image(self, row):
+            img = cv2.imread(self.get_path(row))
+            masked_image = np.zeros((img.shape[0],img.shape[1],4))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            s = img.shape
+            h_center = (2*row.left+row.width)//2
+            v_center = (2*row.top+row.height)//2
+            h_center = max(self.image_size//2,h_center)
+            v_center = max(self.image_size//2,v_center)
+            h_center = min(s[1]-self.image_size//2,h_center)
+            v_center = min(s[0]-self.image_size//2,v_center)
+            
+            # Random Cropping
+            left = h_center - random.randint(self.crop_size//2,self.image_size//2)
+            top = v_center - random.randint(self.crop_size//2,self.image_size//2)
+            right = left + self.crop_size
+            bottom = top + self.crop_size
+            
+            img = self.transforms(image=img)
+            masked_image[:,:,:3] = img['image']
+            
+            # Add Noise to mask - To do
+            masked_image[row.top:row.top+row.height,row.left:row.left+row.width,3]=1
+    
+            img = masked_image[top:bottom,left:right,:]
+            assert img.shape[0]==self.crop_size
+            assert img.shape[1]==self.crop_size
+            assert img.shape[2]==4
+               
+            img /= 255.0
+            
+            if self.transforms:
+                img = self.transforms(image=img)['image']
+                
             if self.add_channel:
                 return img
             
