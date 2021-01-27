d51cc5736282b747041e2cb4649fa31729df6868
OrKatz7 <Or.Katz1@gmail.com>
2021-01-27 13:17:11 +0200

    Delete cnn2d directory

--- cnn2d/train.sh
+++
@@ -1,6 +0,0 @@
-
-python3 train.py --config efficientnetb5 --fold 0
-python3 train.py --config efficientnetb5 --fold 1
-python3 train.py --config efficientnetb5 --fold 2
-python3 train.py --config efficientnetb5 --fold 3
-python3 train.py --config efficientnetb5 --fold 4
